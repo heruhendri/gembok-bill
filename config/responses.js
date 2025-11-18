9acc318a85a2b0ea83b834bb885171892a6df5d1
heruhendri <heruu2004@gmail.com>
2025-11-19 04:35:57 +0700

    change company name

--- config/responses.js
+++ config/responses.js
@@ -4,9 +4,9 @@ const { getSetting } = require('./settingsManager');
 
 // Format pesan dengan header dan footer
 function formatWithHeaderFooter(message) {
-    const COMPANY_HEADER = getSetting('company_header', "📱 ALIJAYA DIGITAL NETWORK 📱\n\n");
+    const COMPANY_HEADER = getSetting('company_header', "📱 HERU HENDRI 📱\n\n");
     const FOOTER_SEPARATOR = "\n\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n";
-    const FOOTER_INFO = FOOTER_SEPARATOR + getSetting('footer_info', "Powered by Alijaya Digital Network");
+    const FOOTER_INFO = FOOTER_SEPARATOR + getSetting('footer_info', "Powered by HENDRI");
     
     return `${COMPANY_HEADER}${message}${FOOTER_INFO}`;
 }
